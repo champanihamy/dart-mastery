210c500bcc1439da566ce5a83adb677f395c8205
champanihamy <wcuhamy@outlook.com>
2026-03-05 23:45:55 +0530

    feat(interface class): implement a interface class

---
+++ class-modifiers/lib/interface_lib.dart
@@ -0,0 +1,6 @@
+/// Outside the library, other developers can see your methods and fields,
+/// but they cannot use extends. They must use implements and write their own logic for every single method.
+
+interface class Encryptor {
+  void encrypt() => print("Standard encryption logic");
+}
